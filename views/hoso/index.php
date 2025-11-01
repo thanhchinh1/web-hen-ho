80f091ce60cf627d95ffb97f0f0e22f45ea6a1c4
NguyenPhuocThinh <thinh98.tt2@gmail.com>
2025-11-01 16:41:16 +0700

    update logo

--- views/hoso/index.php
+++ views/hoso/index.php
@@ -29,9 +29,8 @@ $interests = explode(', ', $profile['soThich']);
 <body>
     <header class="profile-header">
         <a href="../trangchu/index.php" class="logo">
-            <svg viewBox="0 0 24 24" fill="none" class="logo-icon">
-                <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" fill="#ff6b9d"/>
-            </svg>
+            <img src="../../public/img/logo.jpg" alt="Kết Nối Yêu Thương">
+            <span class="logo-text">DuyenHub</span>
         </a>
         <div class="header-center">
             <a href="../trangchu/index.php" class="nav-link">Trang chủ</a>
